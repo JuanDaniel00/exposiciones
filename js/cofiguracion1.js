e9986d9680c097c6201e1bf4d6dde75296e822ec
JuanDaniel00 <juanuribe@colegioguanenta.edu.co>
2022-05-16 17:22:16 -0500

    Desarrollo: Ejercicio 3 exposicion

--- js/cofiguracion1.js
+++
@@ -1,7 +0,0 @@
-let x = myFunction(4, 3);             // Function is called, return value will end up inx
-
-
-function myFunction(a, b){
-
-  returna*b;                          // Function returns the product ofaandb
-}
