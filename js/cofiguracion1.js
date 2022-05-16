233bafe94b921a42d6eb26c511e1999bf79c39b2
sistemas <juanuribe@colegioguanenta.edu.co>
2022-05-16 15:43:43 -0500

    Desarrollo: Exposicion Sierra

---
+++ js/cofiguracion1.js
@@ -0,0 +1,7 @@
+let x = myFunction(4, 3);             // Function is called, return value will end up inx
+
+
+function myFunction(a, b){
+
+  returna*b;                          // Function returns the product ofaandb
+}
